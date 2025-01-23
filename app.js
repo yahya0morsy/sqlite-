4d937a8b959691f584872884820ac2783a0705f2
yahya0morsy <ym60307@gmail.com>
2025-01-24 00:59:21 +0200

    first4 commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose
 
 
 // List of allowed origins
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000', 'https://bonus-front.vercel.app'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000', 'https://bonus-front.vercel.app/'];
 
 // Custom CORS configuration
 const corsOptions = {
